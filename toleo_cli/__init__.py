64d198aefdfb201c724d42a5e2866f4eea3eecc1
cgtx <carl@cgtx.us>
2014-11-20 13:33:33 -0600

    read and process config

--- toleo_cli/__init__.py
+++ toleo_cli/__init__.py
@@ -1,4 +1,5 @@
 import click
+import multiprocessing
 import pathlib
 import prettytable
 import toleo
@@ -28,10 +29,45 @@ def read_config(collection='default'):
         raise FileNotFoundError('cannot read {}'.format(config_file))
 
 
+def worker(item):
+    name, data = item
+    src = data.get('src')
+    pkg = data.get('pkg')
+
+    src_name = src.pop('name', name)
+    src_type = src.pop('type', 'generic')
+    if src_type == 'generic':
+        software = toleo.GenericSoftware(src_name, **src)
+    elif src_type == 'pypi':
+        software = toleo.PypiSoftware(src_name, **src)
+    elif src_type == 'github':
+        software = toleo.GithubSoftware(src_name, **src)
+    elif src_type == 'bitbucket':
+        software = toleo.BitbucketSoftware(src_name, **src)
+
+    pkg_name = pkg.pop('name', name)
+    pkg_type = pkg.pop('type')
+    if pkg_type == 'aur':
+        package = toleo.AurPackage(pkg_name, **pkg)
+
+    return (software, package)
+
+
 @click.command()
 @click.option('--collection', '-c', default='default')
 def cli(collection):
     ''' Entry point for application. '''
-    table = SoftwareTable(['NAME', 'PROJECT', 'REPO', 'SOURCE', 'STATUS'])
+
+    config = read_config()
+
+    pool = multiprocessing.Pool()
+    results = pool.map(worker, config.items())
+    pool.close()
+    pool.join()
+
+    table = SoftwareTable(['PACKAGE', 'PROJECT', 'REPO', 'SOURCE', 'STATUS'])
+    for result in results:
+        software, package = result
+        table.add_software_row(package, software)
 
     print(table)
