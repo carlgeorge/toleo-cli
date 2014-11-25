b98d9a396b793f20aaa92d896104ddd77ed34eae
cgtx <carl@cgtx.us>
2014-11-25 13:33:43 -0600

    move processing to toleo library

--- toleo_cli/__init__.py
+++ toleo_cli/__init__.py
@@ -1,6 +1,6 @@
 import click
+import toleo
 from .config import read_config
-from .multi import handle
 from .table import create_table
 
 
@@ -8,7 +8,7 @@ from .table import create_table
 @click.option('--collection', '-c', default='default')
 def cli(collection):
     ''' Entry point for application. '''
-    config = read_config()
-    results = handle(config)
+    config = read_config(collection)
+    results = toleo.process(config)
     table = create_table(results)
     print(table)
