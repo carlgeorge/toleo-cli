e10ff2a7b11c93e85fb5d749ad12c1ae786a6a2b
cgtx <carl@cgtx.us>
2014-11-27 10:50:49 -0600

    explicitly call evr or version instead of relying on __str__

--- toleo_cli/table.py
+++ toleo_cli/table.py
@@ -2,13 +2,15 @@ import prettytable
 
 
 class SoftwareTable(prettytable.PrettyTable):
-    def add_software_row(self, pkg, src):
+    def add_software_row(self, src, pkg):
         if src.version > pkg.version:
             status = 'UPDATE'
         else:
             status = 'OK'
-        self.add_row([pkg.name, src.name,
-                      pkg.version, src.version,
+        self.add_row([pkg.name,
+                      src.name,
+                      pkg.version.evr,
+                      src.version.version,
                       status])
 
 
@@ -16,5 +18,5 @@ def create_table(results):
     table = SoftwareTable(['PACKAGE', 'PROJECT', 'REPO', 'SOURCE', 'STATUS'])
     for result in results:
         software, package = result
-        table.add_software_row(package, software)
+        table.add_software_row(software, package)
     return table
